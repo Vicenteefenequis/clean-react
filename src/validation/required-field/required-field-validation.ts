79ea21f0b1e2c55509bc4017e8f350386be542c6
Vicente Nascimento <vicente19981@live.com>
2022-02-01 19:28:09 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/validation/required-field/required-field-validation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { RequiredFieldError } from '@/validation/errors'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly fieldName: string) {}
+  validate (value: String): Error {
+    return new RequiredFieldError()
+  }
+}
