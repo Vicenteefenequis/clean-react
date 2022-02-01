5ca367a4b29b805301b5026def6ed94e2ed5aaf5
Vicente Nascimento <vicente19981@live.com>
2022-01-31 22:00:24 -0300

    refactor: removing comments on code

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Login } from '@/presentation/pages'
 
-// import { Container } from './styles';
-
 ReactDOM.render(<Login/>,
   document.getElementById('main')
 )
