9a4eb27867bd0528eec374b3b83554914861c24b
Vicente Nascimento <vicente19981@live.com>
2022-02-01 14:35:40 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const Login: React.FC = () => {
-  const [state] = useState({
+type Props = {
+  validation: Validation
+}
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
     isLoading: false,
-    errorMessage: '',
-    main: ''
+    email: '',
+    emailError: 'Campo obrigatorio',
+    passwordError: 'Campo obrigatorio',
+    mainError: ''
   })
-  const [errorState] = useState({
-    email: 'Campo obrigatorio',
-    password: 'Campo obrigatorio'
-  })
+
+  useEffect(() => { validation.validate({ email: state.email }) }, [state.email])
 
   return (
     <div className={Styles.login}>
       <LoginHeader/>
-      <Context.Provider value={{ state, errorState }}>
+      <Context.Provider value={{ state, setState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
 
