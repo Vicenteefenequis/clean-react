5d143ab8de973c93fc0db438a1dbe7e8fda9e046
Vicente Nascimento <vicente19981@live.com>
2022-02-13 14:05:57 -0300

    feat: ensure Login calls SaveAccessToken with correct value

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,36 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import Styles from './login-styles.scss'
-import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
-import Context from '@/presentation/contexts/form/form-context'
-import { Validation } from '@/presentation/protocols/validation'
-import { Authentication } from '@/domain/usecases'
-import { Link, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from "react"
+import Styles from "./login-styles.scss"
+import {
+  Footer,
+  Input,
+  LoginHeader,
+  FormStatus,
+} from "@/presentation/components"
+import Context from "@/presentation/contexts/form/form-context"
+import { Validation } from "@/presentation/protocols/validation"
+import { Authentication, SaveAccessToken } from "@/domain/usecases"
+import { Link, useHistory } from "react-router-dom"
 
 type Props = {
   validation: Validation
   authentication: Authentication
+  saveAccessToken: SaveAccessToken
 }
-const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+const Login: React.FC<Props> = ({
+  validation,
+  authentication,
+  saveAccessToken,
+}: Props) => {
   const history = useHistory()
 
   const [state, setState] = useState({
     isLoading: false,
-    email: '',
-    password: '',
-    emailError: '',
-    passwordError: '',
-    mainError: ''
+    email: "",
+    password: "",
+    emailError: "",
+    passwordError: "",
+    mainError: "",
   })
 
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
+      emailError: validation.validate("email", state.email),
+      passwordError: validation.validate("password", state.password),
     })
   }, [state.email, state.password])
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
     event.preventDefault()
     try {
       if (state.isLoading || state.emailError || state.passwordError) {
@@ -39,11 +51,11 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
       setState({ ...state, isLoading: true })
       const account = await authentication.auth({
         email: state.email,
-        password: state.password
+        password: state.password,
       })
 
-      localStorage.setItem('accessToken', account.accessToken)
-      history.replace('/')
+      await saveAccessToken.save(account.accessToken)
+      history.replace("/")
     } catch (error) {
       setState({ ...state, isLoading: false, mainError: error.message })
     }
@@ -51,21 +63,37 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
 
   return (
     <div className={Styles.login}>
-      <LoginHeader/>
+      <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form data-testid="form" className={Styles.form} onSubmit={handleSubmit}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Login</h2>
 
-          <Input type="email" name="email" placeholder='Digite seu e-mail'/>
-          <Input type="password" name="password" placeholder='Digite sua senha'/>
+          <Input type="email" name="email" placeholder="Digite seu e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
 
-          <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type='submit'>Entrar</button>
-          <Link data-testid="signup" to="/signup" className={Styles.link}>Criar conta</Link>
-          <FormStatus/>
-
+          <button
+            data-testid="submit"
+            disabled={!!state.emailError || !!state.passwordError}
+            className={Styles.submit}
+            type="submit"
+          >
+            Entrar
+          </button>
+          <Link data-testid="signup" to="/signup" className={Styles.link}>
+            Criar conta
+          </Link>
+          <FormStatus />
         </form>
       </Context.Provider>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
