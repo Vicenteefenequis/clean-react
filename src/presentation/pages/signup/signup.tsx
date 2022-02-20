2c891857ba3e24d6a6b9460287b244ccda4421f0
Vicente Nascimento <vicente19981@live.com>
2022-02-20 14:29:25 -0300

    feat: ensure SignUp start with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './signup-styles.scss'
 import {
   Footer,
@@ -10,10 +10,19 @@ import Context from '@/presentation/contexts/form/form-context'
 import { Link } from 'react-router-dom'
 
 const SignUp: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    nameError: 'Campo obrigatorio',
+    emailError: 'Campo obrigatorio',
+    passwordError: 'Campo obrigatorio',
+    passwordConfirmationError: 'Campo obrigatorio',
+    mainError: ''
+  })
+
   return (
     <div className={Styles.signup}>
       <LoginHeader />
-      <Context.Provider value={{ state: {} }}>
+      <Context.Provider value={{ state }}>
         <form className={Styles.form}>
           <h2>Criar Conta</h2>
 
@@ -30,12 +39,15 @@ const SignUp: React.FC = () => {
             placeholder="Repita sua senha"
           />
 
-          <button className={Styles.submit} type="submit">
+          <button
+            data-testid="submit"
+            disabled
+            className={Styles.submit}
+            type="submit"
+          >
             Entrar
           </button>
-          <Link to="/login" className={Styles.link}>
-            Voltar Para Login
-          </Link>
+          <span className={Styles.link}>Voltar Para Login</span>
           <FormStatus />
         </form>
       </Context.Provider>
