6068780445075a0e15f621e46d01df9c1bda6e8a
Vicente Nascimento <vicente19981@live.com>
2022-02-21 13:13:17 -0300

    refactor: change implementation test error

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -1,4 +1,4 @@
-import { InvalidFieldError, RequiredFieldError } from '@/validation/errors'
+import { InvalidFieldError } from '@/validation/errors'
 import faker from 'faker'
 import { CompareFieldsValidation } from './compare-fields-validation'
 
@@ -10,13 +10,15 @@ const makeSut = (
 
 describe('CompareFieldsValidation', () => {
   test('Should return error if compare is invalid', () => {
-    const fieldName = faker.database.column()
-    const fieldToCompare = faker.database.column()
-    const sut = makeSut(fieldName, fieldToCompare)
+    const field = 'any_field'
+    const fieldToCompare = 'other_field'
+    const sut = makeSut(field, fieldToCompare)
+
     const error = sut.validate({
-      [fieldName]: faker.database.column(),
-      [fieldToCompare]: faker.database.column()
+      [field]: 'any_value',
+      [fieldToCompare]: 'other_value'
     })
+
     expect(error).toEqual(new InvalidFieldError())
   })
   test('Should return falsy if compare is valid', () => {
