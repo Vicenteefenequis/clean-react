2777703baa1c8652611b985c481c17b9444d11ad
Vicente Nascimento <vicente19981@live.com>
2022-03-14 15:49:41 -0300

    refactor: move SurveyList code to components

---
+++ src/presentation/pages/survey-list/components/list/list.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import {
+  SurveyItem,
+  SurveyItemEmpty,
+  SurveyContext
+} from '@/presentation/pages/survey-list/components'
+import Styles from './list-styles.scss'
+
+const List: React.FC = () => {
+  const { state } = useContext(SurveyContext)
+  return (
+    <ul className={Styles.listWrap} data-testid="survey-list">
+      {state.surveys.length ? (
+        state.surveys.map(survey => (
+          <SurveyItem key={survey.id} survey={survey} />
+        ))
+      ) : (
+        <SurveyItemEmpty />
+      )}
+    </ul>
+  )
+}
+
+export default List
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,9 @@
 import { Footer, Header } from '@/presentation/components'
 import React, { useEffect, useState } from 'react'
 import {
-  SurveyItem,
-  SurveyItemEmpty
+  SurveyContext,
+  SurveyListItem,
+  Error
 } from '@/presentation/pages/survey-list/components'
 import Styles from './survey-list-styles.scss'
 import { LoadSurveyList } from '@/domain/usecases'
@@ -32,22 +33,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        {state.error ? (
-          <div>
-            <span data-testid="error">{state.error}</span>
-            <button>Recarregar</button>
-          </div>
-        ) : (
-          <ul data-testid="survey-list">
-            {state.surveys.length ? (
-              state.surveys.map(survey => (
-                <SurveyItem key={survey.id} survey={survey} />
-              ))
-            ) : (
-              <SurveyItemEmpty />
-            )}
-          </ul>
-        )}
+        <SurveyContext.Provider value={{ state, setState }}>
+          {state.error ? <Error /> : <SurveyListItem />}
+        </SurveyContext.Provider>
       </div>
       <Footer />
     </div>
