3209269272213f7b5c46a4b75aa2e9fff3e66135
Vicente Nascimento <vicente19981@live.com>
2022-03-17 21:37:16 -0300

    test: ensure SurveyList logout on AccessDeniedError

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -9,6 +9,7 @@ describe('SurveyList', () => {
       name: faker.name.findName()
     })
   })
+
   it('Should present error on UnexpectedError', () => {
     Http.mockUnexpectedError()
     cy.visit('')
@@ -17,5 +18,12 @@ describe('SurveyList', () => {
       'contain.text',
       'Algo de errado aconteceu. Tente novamente em breve'
     )
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    Http.mockAccessDeniedError()
+    cy.visit('')
+
+    Helper.testUrl('/login')
   })
 })
